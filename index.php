00b8324b48e6e47e21e82c7b37c2b4b7533300e1
Zaysman <39443734+Zaysman@users.noreply.github.com>
2021-03-16 15:46:43 -0400

    Add files via upload

---
+++ index.php
@@ -0,0 +1,10 @@
+<html>
+<head>
+<title>
+    Index.html
+</title>
+</head>
+<body>
+    If you see this page, you've logged on.
+</body>
+</html>
--- signup.php
+++ signup.php
@@ -1,59 +1,109 @@
-<!-- shared meta data HTML -->
-<?php include 'meta-data.html'; ?>
+<?php session_start(); /*start session */
+    if(isset($_POST['submit'])) {
+        //Check if password and confirm_password are the same.
+        /* Check and assign submitted Username and Password to new variable 
+		$username = isset($_POST['username']) ? $_POST['username'] : '';
+		$password = isset($_POST['password']) ? $_POST['password'] : '';
+        $confirm_password = isset($_Post['confirm_password']) ? $_POST['confirm_passowrd'] : '';
+        */
+        $username = $_REQUEST['username'];
+        $password = $_REQUEST['password'];
+        $confirm_password = $_REQUEST['confirm_password'];
 
+
+
+        if(strcmp($password, $confirm_password) !== 0) {   
+            $str = nl2br($password . "," . $confirm_password);
+            print($str);
+
+
+        } else {
+            $write_str = nl2br($username . "," . $password . "\n");
+            file_put_contents("./text_files/accounts.txt", $write_str, FILE_APPEND);
+            echo "Thank you for signing up!";
+        }
+    }
+
+
+
+?>
+
+
+<!doctype html>
+<html lang="en">
+<head>
+<title>
+    Sign Up Page
+</title>
+<link rel="stylesheet" href="./style.css">
+</head>
 <body>
-	<!-- shared banner HTML -->
-	<div id="bannerarea">
-		<?php include 'common_header.html';?>
-	</div>
+Sign up page goes here
 
-	<!-- your HTML output follows -->
+    <h1 class="title">Sign up!</h1>
 
-	<form action="signup-submit.php" method="post" name="signup">
-		<fieldset>
-			<legend>New User Signup:</legend>
-			
-			<strong class="column">Name:</strong>
-			<input type="text" name="name" size="16"/> <br/><br/>
-			
-			<strong class="column">Gender:</strong>
-			<label><input type="radio" name="gender" value="M"/>Male</label>
-			<label><input type="radio" name="gender" value="F" checked="checked"/>Female</label> <br/><br/>
+    <div class="inputs">
+        <form class="input" action="" method="POST" name="signup_form">
+            <fieldset>
+                <table>
+                    <!--Username-->
+                    <tr>
+                        <td>
+                            <label for="username">Username:</label>
+                        </td>
 
-			<strong class="column">Age:</strong>
-			<input type="text" name="age" maxlength = "2" size="6"/> <br/><br/>
-			
-			<strong class="column">Personality Type:</strong>
-			<input type="text" name="personality" maxlength = "4" size="6"/>
-			<a href="http://www.humanmetrics.com/cgi-win/JTypes2.asp">
-				Don't know your type?
-			</a> <br/><br/>
+                        <td>
+                            <input name="username" type="text" size="16">
+                        </td>
+                    </tr>
 
-			<strong class="column">Favorite OS:</strong>
-			<select name="fav_os">
-				<option>Windows</option>
-				<option>Mac OS X</option>
-				<option>Linux</option>
-			</select> <br/><br/>
-			
-			<strong class="column">Seeking age:</strong>
-			<input type="text" name="age_min" placeholder="min" maxlength = "2" size="6"/>
-			to
-			<input type="text" name="age_max" placeholder="max" maxlength = "2" size="6"/> <br/><br/>
+                    <!--Password row-->
+                    <tr>
+                        <td>
+                            <label for="password">Password:</label>
+                        </td>
+    
+                        <td>
+                            <input name="password" type="text" size="16">
+                        </td>
+    
+                    </tr>
 
-			<strong class="column">Seek Gender(s):</strong>
-			<input type="checkbox" id="seek_male" name="seek_male" value="M" checked="checked">
-			<label for="seek_male"> Male</label>
-			<input type="checkbox" id="seek_female" name="seek_female" value="F">
-			<label for="seek_female"> Female</label><br/><br/>
 
-			<input type="submit" value="Sign Up"/>
-		</fieldset>
-	</form>
+                    <!--Confirm password row-->
+                    <tr>
+                        <td>
+                            <label for="confirm_password">Confirm Password:</label>
+                        </td>
+    
+                        <td>
+                            <input id="confirm_password" name="confirm_password" type="text" size="16">
+                        </td>
+                    </tr>
 
-	<!-- shared page bottom HTML -->
-	<div>
-		<?php include 'common.php';?>
-	</div>
+                    <!--Submit row-->
+                    <tr>
+                        <td colspan="2" class="centered">
+                            <input id="submit-btn" class="submit-btn" name="submit" type="submit" value="Sign up!">
+                        </td>
+
+                    </tr>
+                    
+                    <!--Go to login-->
+                    <tr>
+                        <td colspan="2" class="centered" align="center">
+                            <a href="login.php">Login in</a>
+                        </td>
+                    </tr>
+                </table>
+
+
+
+            </fieldset>
+
+        </form>
+    </div>
+
+
 </body>
 </html>
